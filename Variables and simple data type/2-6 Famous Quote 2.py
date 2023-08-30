8d814df019f6c03000855952696b7c69752f35f0
Carlos Alberto Palacio Acuña <119970552+CAPalacio03@users.noreply.github.com>
2023-08-29 20:24:35 -0600

    Add files via upload

---
+++ Variables and simple data type/2-6 Famous Quote 2.py
@@ -0,0 +1,10 @@
+'''
+Repeat Exercise 2-5, but this time, represent the famous person’s name using a 
+variable called famous_person. Then compose your message and represent it with a 
+new variable called message. Print your message.
+'''
+famous_person = 'Sirius Black'
+quote = '\"The world isn´t split it into good people and bad people. We´ve all got both light\n\
+and dark inside of us. What matters is the part we choose to act on. that who we really are\"'
+
+print(f'{famous_person} said: \n {quote}')
