1944f80c7aa62a6e1a3839010e3a5cbc92f0d29d
CAPalacio03 <caalpa007@gmail.com>
2025-10-05 18:52:42 -0600

    Ejercicios de evoiding index errors when working with lists

---
+++ 3. introducing_lits/3-11 Intentional Error.py
@@ -0,0 +1,9 @@
+"""
+If you haven't received an index error in one of your programs yet, try to make one happen.
+Change an index in one of your programs to prodice an index error. Make sure you correct the
+error before closing the program 
+"""
+
+languages = ['Spanish', 'English', 'French', 'Chinese', 'German', 'Italian']
+
+print(languages[7])
