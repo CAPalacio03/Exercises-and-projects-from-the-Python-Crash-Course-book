8217f662f2e8605393a74cb77912ebfb0fb6d1a1
CAPalacio03 <caalpa007@gmail.com>
2025-10-07 14:54:02 -0600

    Ejercicos Looping Through an Entire List

---
+++ 4. Working with Lists/4-1 Pizzas.py
@@ -0,0 +1,16 @@
+"""
+Think of at least three kinds of your favorite pizza. Store these 
+pizza names in a list, and then use a for loop to print the name of each pizza.
+"""
+
+pizzas = ['Perperoni', 'Mix', 'Mexicana']
+
+for pizza in pizzas:
+    print(pizza)
+
+# Modify your for loop to print a sentence using the name of the pizza 
+for pizza in pizzas:
+    print(f'I like {pizza} pizza')
+
+# Add a line at the end of your program, outside the for loop, that states how much you like pizza
+print('I really like pizze')
---
+++ 4. Working with Lists/4-2 Animals.py
@@ -0,0 +1,17 @@
+"""
+Think of at least three different animals that have a common char
+acteristic. Store the names of these animals in a list, and then use a for loop to 
+print out the name of each animal
+"""
+
+animals = ['Leon', 'Cat', 'Panther']
+
+for animal in animals:
+    print(animal)
+
+# Modify your program to print a statement about each animal
+for animal in animals:
+    print(f'The {animal} is a Feline')
+
+# Add a line at the end of your program stating what these animals have in common 
+print('They have claws')
